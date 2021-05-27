ed6f95a593873bca584e84b7529c2c15ebe32da8
Kelvince1 <themirkochip1@gmail.com>
2021-05-27 15:55:40 +0300

    Add project files.

---
+++ TwilioSMSConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwilioSMSConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account SID and Auth Token at twilio.com/console
+            // and set the environment variables. See http://twil.io/secure
+            string accountSid = "AC70053d032854e552cf7173c0f982a108";
+            string authToken = "605d4c7dc121abcfbbd06c4c14b1d1c2";
+
+            /*string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");*/
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                from: new Twilio.Types.PhoneNumber("+18595442417"),
+                to: new Twilio.Types.PhoneNumber("+254790292111")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
